05c51ebded553c3fc44e21ccc1eae9497d71f505
Luciano <l_baarn@hotmail.com>
2022-03-10 10:23:39 +0100

    Add files via upload

---
+++ src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+      <page-header />
+
+      <page-footer />
+  </div>
+</template>
+
+<script>
+import PageHeader from './PageHeader.vue'
+import PageFooter from './PageFooter.vue'
+
+export default {
+    components: {
+        PageHeader, PageFooter
+    }
+}
+</script>
+
+<style>
+
+</style>
